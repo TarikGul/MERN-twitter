d95ebc0f8017e0615f3ad63f318781d93a845e6d
Tarik <tariksnow37@gmail.com>
2020-03-14 22:02:18 -0700

    Added tweets and user auth

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
-const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
-const port = process.env.PORT || 5000;
+const mongoose = require("mongoose");
+
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets") 
-const bodyParser = require('body-parser');
 
 mongoose
     .connect(db, {useNewUrlParser: true})
     .then(() => console.log("connected to the MongoDB successfully"))
     .catch(err => console.log(err));
 
-    app.use("/api/users", users);
-    app.use("/api/tweets", tweets);
-    app.use(bodyParser.urlencoded({ extended: false }));
-    app.use(bodyParser.json());
-    app.get("/", (req, res) => res.send("Welcome to the Express Setup"));
+app.get("/", (req, res) => {
+    res.send("Welcome to the Express Setup");
+});
 
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on part ${port}`));
