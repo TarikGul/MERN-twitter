efacf95e3e075a7e1ec9d877157a1d55b942d153
Tarik <tariksnow37@gmail.com>
2020-03-17 09:26:33 -0700

    added comment

--- config/key_dev.js
+++ config/key_dev.js
@@ -2,3 +2,4 @@ module.exports = {
     mongoURI: 'mongodb+srv://sf-sunset-dev:w5XYpMFhQIjEObgw@mern-z7gr4.mongodb.net/test?retryWrites=true&w=majority',
     secretOrKey: '8_%{3_F!^TIpJL`'
 }
+// helllo
